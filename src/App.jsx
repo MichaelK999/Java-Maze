8fac2f405c9a9ffdc31789e25b6d695934aa797b
Michael Kapustin <Michaelkapustin1@gmail.com>
2025-11-07 10:03:45 +0000

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         
         {/* Game Title */}
         <div className="flex items-center gap-4">
-          <img src="/enemy.png" alt="Enemy" className="w-10 h-10" />
+          <img src="enemy.png" alt="Enemy" className="w-10 h-10" />
           <h1 className="text-5xl font-bold tracking-wide text-gray-900">
             UNI MAZE
           </h1>
