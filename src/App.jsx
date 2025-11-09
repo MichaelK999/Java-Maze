82212e26bc40e4f6e61d5eebeb944768dc681ef5
benbur <xdj514@york.ac.uk>
2025-11-09 18:12:38 +0000

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <div className="flex items-center gap-4">
           <img src="enemy.png" alt="Enemy" className="w-10 h-10" />
           <h1 className="text-5xl font-bold tracking-wide text-gray-900">
-            UNI MAZE
+            GRADUATION GAUNTLET
           </h1>
         </div>
         
